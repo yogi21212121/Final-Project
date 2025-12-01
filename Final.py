eea01be17a30b9cf74b81f39470c1ed9e9479a3c
yogi21212121 <osielrodriguez420@gmail.com>
2025-11-30 22:38:33 -0700

    added another program with sleep.time

---
+++ Final.py
@@ -0,0 +1,38 @@
+import time
+import datetime
+import pygame
+
+
+def say_name(name):
+    print("Your name is:", name)
+
+user_name = input("Enter your name: ")
+
+def set_alarm(alarm_time):
+    print(f"Alarm set for {alarm_time}")
+    sound_file = "alarm.mp3"
+    is_running = True
+
+    while is_running:
+        current_time = datetime.datetime.now().strftime("%H:%M:%S")
+        print(current_time)
+
+        if current_time == alarm_time:
+            print("WAKE UP! 😴")
+
+            pygame.mixer.init()
+            pygame.mixer.music.load(sound_file)
+            pygame.mixer.music.play()
+
+            while pygame.mixer.music.get_busy():
+                time.sleep(1)
+
+            is_running = False
+
+        time.sleep(1)
+
+
+if __name__ == "__main__":
+    say_name(user_name)
+    alarm_time = input("Enter the alarm time (HH:MM:SS): ")
+    set_alarm(alarm_time)
